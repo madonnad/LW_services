ff43b72d2096d4bf605689b4b0613783aa9a6fb0
Jamie Kuppel <jamie@vannevarlabs.com>
2023-09-18 17:51:36 -0400

    Bubble up errors

--- src/handlers/ws_handler.go
+++ src/handlers/ws_handler.go
@@ -34,23 +34,26 @@ func WebSocketHandler(w http.ResponseWriter, r *http.Request, connPool *m.PGPool
 	log.Print("Listening via WebSocket...")
 	queryTime := time.Now().UTC()
 	updatedTime := queryTime
-
 	for {
-		AlbumRequestCheck(ctx, connPool, conn, &queryTime, &updatedTime)
-		FriendRequestCheck(ctx, connPool, conn, &queryTime, &updatedTime)
-		NotificationCheck(ctx, connPool, conn, &queryTime, &updatedTime)
-
+		if err := AlbumRequestCheck(ctx, connPool, conn, &queryTime, &updatedTime); err != nil {
+			log.Printf("failed to check album notifications: %s", err)
+		}
+		if err := FriendRequestCheck(ctx, connPool, conn, &queryTime, &updatedTime); err != nil {
+			log.Printf("failed to check friend requests: %s", err)
+		}
+		if err := NotificationCheck(ctx, connPool, conn, &queryTime, &updatedTime); err != nil {
+			log.Printf("failed to check generic notifications: %s", err)
+		}
 		time.Sleep(4 * time.Second)
 		queryTime = updatedTime
 	}
 
 }
 
-func AlbumRequestCheck(ctx context.Context, connPool *m.PGPool, conn *websocket.Conn, queryTime *time.Time, updatedTime *time.Time) {
+func AlbumRequestCheck(ctx context.Context, connPool *m.PGPool, conn *websocket.Conn, queryTime *time.Time, updatedTime *time.Time) error {
 	var wsPayload WebSocketPayload
 	var album Album
 	var receivedLocal time.Time
-
 	notificationQuery := `SELECT a.album_id, a.album_name, a.album_cover_id, a.album_owner, ar.invited_at
 						 FROM albums a
 						 JOIN album_requests ar ON a.album_id = ar.album_id
@@ -58,16 +61,15 @@ func AlbumRequestCheck(ctx context.Context, connPool *m.PGPool, conn *websocket.
 
 	rows, err := connPool.Pool.Query(ctx, notificationQuery, uid, *queryTime)
 	if err != nil {
-		log.Print(err)
+		return err
 	}
-
 	for rows.Next() {
 		wsPayload.Type = "album_request"
 		wsPayload.Operation = "INSERT"
 
 		err := rows.Scan(&album.AlbumID, &album.AlbumName, &album.AlbumCoverID, &album.AlbumOwner, &receivedLocal)
 		if err != nil {
-			log.Print(err)
+			return err
 		}
 
 		wsPayload.Payload = album
@@ -76,34 +78,30 @@ func AlbumRequestCheck(ctx context.Context, connPool *m.PGPool, conn *websocket.
 		if wsPayload.Received.After(*updatedTime) {
 			*updatedTime = wsPayload.Received
 		}
-		HandleNotifications(conn, wsPayload)
+		writeNotification(conn, wsPayload)
 	}
+	return nil
 }
 
-func FriendRequestCheck(ctx context.Context, connPool *m.PGPool, conn *websocket.Conn, queryTime *time.Time, updatedTime *time.Time) {
+func FriendRequestCheck(ctx context.Context, connPool *m.PGPool, conn *websocket.Conn, queryTime *time.Time, updatedTime *time.Time) error {
 	var wsPayload WebSocketPayload
 	var user User
 	var receivedLocal time.Time
-
-	log.Print(queryTime)
-
 	notificationQuery := `SELECT fr.sender_id, u.first_name, u.last_name, fr.requested_at
 						  FROM users u
 						  JOIN friend_requests fr ON fr.sender_id = u.user_id
 						  WHERE fr.receiver_id = $1 AND fr.requested_at > $2`
-
 	rows, err := connPool.Pool.Query(ctx, notificationQuery, uid, *queryTime)
 	if err != nil {
-		log.Print(err)
+		return err
 	}
-
 	for rows.Next() {
 		wsPayload.Type = "friend_request"
 		wsPayload.Operation = "INSERT"
 
 		err := rows.Scan(&user.ID, &user.FirstName, &user.LastName, &receivedLocal)
 		if err != nil {
-			log.Print(err)
+			return err
 		}
 
 		wsPayload.Payload = user
@@ -112,16 +110,18 @@ func FriendRequestCheck(ctx context.Context, connPool *m.PGPool, conn *websocket
 		if wsPayload.Received.After(*updatedTime) {
 			*updatedTime = wsPayload.Received
 		}
-		HandleNotifications(conn, wsPayload)
+		if err := writeNotification(conn, wsPayload); err != nil {
+			return err
+		}
 	}
+	return nil
 }
 
-func NotificationCheck(ctx context.Context, connPool *m.PGPool, conn *websocket.Conn, queryTime *time.Time, updatedTime *time.Time) {
+func NotificationCheck(ctx context.Context, connPool *m.PGPool, conn *websocket.Conn, queryTime *time.Time, updatedTime *time.Time) error {
 	var wsPayload WebSocketPayload
 	var user User
 	var genericNotification m.GenericNotification
 	var receivedLocal time.Time
-
 	notificationQuery := `SELECT n.sender_id, u.first_name, u.last_name, n.media_id, a.album_name, n.notification_type, n.notification_seen, n.received_at
 						  FROM users u
 						  JOIN notifications n ON n.sender_id = u.user_id
@@ -130,16 +130,15 @@ func NotificationCheck(ctx context.Context, connPool *m.PGPool, conn *websocket.
 
 	rows, err := connPool.Pool.Query(ctx, notificationQuery, uid, queryTime)
 	if err != nil {
-		log.Print(err)
+		return err
 	}
-
 	for rows.Next() {
 		wsPayload.Type = "generic_notification"
 		wsPayload.Operation = "INSERT"
 
 		err := rows.Scan(&user.ID, &user.FirstName, &user.LastName, &genericNotification.MediaID, &genericNotification.AlbumName, &genericNotification.NotificationType, &genericNotification.NotificationSeen, &receivedLocal)
 		if err != nil {
-			log.Print(err)
+			return err
 		}
 		genericNotification.Notifier = user
 		wsPayload.Payload = genericNotification
@@ -148,20 +147,21 @@ func NotificationCheck(ctx context.Context, connPool *m.PGPool, conn *websocket.
 		if wsPayload.Received.After(*updatedTime) {
 			*updatedTime = wsPayload.Received
 		}
-		HandleNotifications(conn, wsPayload)
+		if err := writeNotification(conn, wsPayload); err != nil {
+			return err
+		}
 	}
+	return nil
 }
 
-func HandleNotifications(conn *websocket.Conn, n WebSocketPayload) {
+func writeNotification(conn *websocket.Conn, n WebSocketPayload) error {
 	responseBytes, err := json.MarshalIndent(n, "", "\t")
 	if err != nil {
-		log.Print(err)
+		return err
 	}
-
 	err = conn.WriteMessage(websocket.TextMessage, responseBytes)
-	log.Printf("Sent: %v, Operation: %v, Received At: %v\n", n.Type, n.Operation, n.Received)
-	if err != nil {
-		log.Print(err)
+	if err == nil {
+		log.Printf("Sent: %v, Operation: %v, Received At: %v\n", n.Type, n.Operation, n.Received)
 	}
-
+	return err
 }
