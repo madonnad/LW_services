8900031752eec28a32777ea6a0ff3de07bc6b621
Dominick Madonna <dominick.madonn@gmail.com>
2023-09-20 18:30:23 -0400

    Added the middleman to check for valid auth token coming through the REST API

--- src/handlers/user_handlers.go
+++ src/handlers/user_handlers.go
@@ -31,13 +31,6 @@ func UserEndpointHandler(connPool *m.PGPool) http.HandlerFunc {
 	})
 }
 
-/*var UserEndpointHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		GETUserInformation(w, r, connPool)
-	}
-})*/
-
 func GETUserInformation(w http.ResponseWriter, r *http.Request, connPool *m.PGPool) {
 	var user User
 
