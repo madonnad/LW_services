1cf0d6302c2da784f46c722888503b7a83a3a889
Dominick Madonna <dominick.madonn@gmail.com>
2023-10-17 11:45:09 -0400

    Created the OpenSearch MultiMatch query - searching on Name, First Name and Last Name - query type is bool_prefix to handle search while typing. Mapped this query into OpenSearch result struct and then mapped that to the Search class to be returned to the user as JSON.

--- src/models/search.go
+++ src/models/search.go
@@ -1,11 +1,34 @@
 package models
 
 type Search struct {
-	ID         string  `json:"-"`
+	ID         string  `json:"id"`
 	Name       string  `json:"name"`
-	Asset      string  `json:"-"`
+	Asset      string  `json:"asset"`
 	FirstName  string  `json:"first_name"`
 	LastName   string  `json:"last_name"`
 	ResultType string  `json:"type"`
-	Rank       float32 `json:"-"`
+	Score      float64 `json:"score"`
+}
+
+type SearchResults struct {
+	MaxScore float64 `json:"max_score"`
+	Hits     Hits    `json:"hits"`
+}
+
+type Hits struct {
+	Hits []Hit `json:"hits"`
+}
+
+type Hit struct {
+	Index  string  `json:"_index"`
+	ID     string  `json:"_id"`
+	Score  float64 `json:"_score"`
+	Source Source  `json:"_source"`
+}
+
+type Source struct {
+	Name       string `json:"name"`
+	FirstName  string `json:"first_name"`
+	LastName   string `json:"last_name"`
+	ResultType string `json:"type"`
 }
