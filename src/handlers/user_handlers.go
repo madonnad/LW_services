bd665833746377d7d38083754b77d3c37202bde1
Dominick Madonna <dominick.madonn@gmail.com>
2024-03-18 23:20:51 -0400

    Created the endpoint for creating the new user in the user table in the database. Also configured the feed handler to gracefully manage no found albums bby throwing a 204 status.

--- src/handlers/user_handlers.go
+++ src/handlers/user_handlers.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	jwtmiddleware "github.com/auth0/go-jwt-middleware/v2"
 	"github.com/auth0/go-jwt-middleware/v2/validator"
+	"io"
 	"log"
 	"net/http"
 
@@ -14,7 +15,7 @@ import (
 	"github.com/jackc/pgx/v5"
 )
 
-func UserEndpointHandler(connPool *m.PGPool) http.HandlerFunc {
+func UserEndpointHandler(connPool *m.PGPool, ctx context.Context) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		claims, ok := r.Context().Value(jwtmiddleware.ContextKey{}).(*validator.ValidatedClaims)
 		if !ok {
@@ -23,17 +24,56 @@ func UserEndpointHandler(connPool *m.PGPool) http.HandlerFunc {
 		}
 
 		switch r.Method {
+		case http.MethodPost:
+			POSTNewAccount(ctx, w, r, connPool, claims.RegisteredClaims.Subject)
 		case http.MethodGet:
 			GETAuthUserInformation(w, r, connPool, claims.RegisteredClaims.Subject)
 		}
 	})
 }
 
+func POSTNewAccount(ctx context.Context, w http.ResponseWriter, r *http.Request, connPool *m.PGPool, uid string) {
+	var user m.User
+
+	bytes, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		WriteErrorToWriter(w, "Error: Could not read the request body")
+		log.Printf("Failed Reading Body: %v", err)
+		return
+	}
+
+	err = json.Unmarshal(bytes, &user)
+	if err != nil {
+		WriteErrorToWriter(w, "Error: Invalid request body - could not be mapped to object")
+		log.Printf("Failed Unmarshaling: %v", err)
+		return
+	}
+
+	query := `INSERT INTO users (first_name, last_name, auth_zero_id) VALUES ($1, $2, $3)`
+
+	_, err = connPool.Pool.Exec(ctx, query, user.FirstName, user.LastName, uid)
+	if err != nil {
+		WriteErrorToWriter(w, "Unable to create new user entry")
+		log.Printf("Unable to create new user entry: %v", err)
+		return
+	}
+
+	responseBytes, err := json.MarshalIndent("account created - success", "", "\t")
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(responseBytes)
+}
+
 func GETAuthUserInformation(w http.ResponseWriter, r *http.Request, connPool *m.PGPool, uid string) {
 	var user m.User
 
-	sql_query := "SELECT user_id, created_at, first_name, last_name FROM users WHERE auth_zero_id=$1"
-	response := connPool.Pool.QueryRow(context.Background(), sql_query, uid)
+	sqlQuery := "SELECT user_id, created_at, first_name, last_name FROM users WHERE auth_zero_id=$1"
+	response := connPool.Pool.QueryRow(context.Background(), sqlQuery, uid)
 
 	//fmt.Printf("%v", response.Scan())
 	err := response.Scan(&user.ID, &user.CreatedAt, &user.FirstName, &user.LastName)
