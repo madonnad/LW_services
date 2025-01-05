5afb50e4e2187eaf8ce028bfdeed3f33c5ea66fe
Dominick Madonna <dominick.madonn@gmail.com>
2025-01-04 21:14:58 -0500

    Made changes to support the change of which time is used for images - in order for this update to work the following changes are required to be made to the database. captured_at datetime added to db with default being current time, update all existing captured_at's (after update) to be the original created_at time.

--- src/handlers/image_handlers.go
+++ src/handlers/image_handlers.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"net/http"
 	"sort"
+	"time"
 
 	jwtmiddleware "github.com/auth0/go-jwt-middleware/v2"
 	"github.com/auth0/go-jwt-middleware/v2/validator"
@@ -797,7 +798,7 @@ func GETImagesFromUserID(ctx context.Context, w http.ResponseWriter, r *http.Req
 
 	for result.Next() {
 		var image m.Image
-		err := result.Scan(&image.ID, &image.ImageOwner, &image.Caption, &image.UploadType, &image.CreatedAt)
+		err := result.Scan(&image.ID, &image.ImageOwner, &image.Caption, &image.UploadType, &image.CapturedAt)
 		if err != nil {
 			log.Print(err)
 		}
@@ -831,7 +832,7 @@ func GETImageFromID(ctx context.Context, w http.ResponseWriter, r *http.Request,
 			  FROM images WHERE image_id = $1`
 
 	results := connPool.Pool.QueryRow(ctx, query, uid)
-	err = results.Scan(&image.ID, &image.ImageOwner, &image.Caption, &image.Upvotes, &image.CreatedAt)
+	err = results.Scan(&image.ID, &image.ImageOwner, &image.Caption, &image.Upvotes, &image.CapturedAt)
 	if err != nil {
 		if err == pgx.ErrNoRows {
 			WriteErrorToWriter(w, "Error: Image does not exist")
@@ -924,14 +925,26 @@ func POSTNewImage(ctx context.Context, w http.ResponseWriter, r *http.Request, c
 			} else {
 				fmt.Println("upload type not defined")
 			}
+		case "captured_at":
+			if capturedAt, ok := value.(string); ok {
+				layout := time.RFC3339 // For ISO8601 format
+				parsedTime, err := time.Parse(layout, capturedAt)
+				if err != nil {
+					fmt.Println("Error parsing datetime:", err)
+				} else {
+					image.CapturedAt = parsedTime
+				}
+			} else {
+				fmt.Println("upload type not defined")
+			}
 		}
 	}
 
 	imageCreationQuery := `INSERT INTO images
-			  (image_id, image_owner, caption, upload_type) VALUES ($1,(SELECT user_id FROM users WHERE auth_zero_id=$2), $3, $4)
+			  (image_id, image_owner, caption, upload_type, captured_at) VALUES ($1,(SELECT user_id FROM users WHERE auth_zero_id=$2), $3, $4, $5)
 			  RETURNING image_id, created_at`
 	err = connPool.Pool.QueryRow(ctx, imageCreationQuery, image.ID, image.ImageOwner, image.Caption,
-		image.UploadType).Scan(&image.ID, &image.CreatedAt)
+		image.UploadType, image.CapturedAt).Scan(&image.ID, &image.CapturedAt)
 	if err != nil {
 		WriteErrorToWriter(w, "Unable to create image in database")
 		log.Printf("Unable to create image in database: %v", err)
@@ -1035,7 +1048,7 @@ func QueryImagesData(ctx context.Context, connPool *m.PGPool, album *m.Album, ui
 		var image m.Image
 
 		err = imageResponse.Scan(&image.ID, &image.ImageOwner, &image.FirstName, &image.LastName, &image.Caption,
-			&image.UploadType, &image.Likes, &image.Upvotes, &image.UserLiked, &image.UserUpvoted, &image.CreatedAt)
+			&image.UploadType, &image.Likes, &image.Upvotes, &image.UserLiked, &image.UserUpvoted, &image.CapturedAt)
 		if err != nil {
 			log.Print(err)
 		}
